560c59e947276ad8971fc60649d08a2a51e29fe7
rmctj <apevmq@gmail.com>
2022-10-28 21:11:06 +0900

    Add crossbeam.

--- src/main.rs
+++ src/main.rs
@@ -92,6 +92,31 @@ fn main() {
     let upper_left = parse_complext(&args[3]).expect("Cannot parse UPPERLEFT");
     let lower_right = parse_complext(&args[4]).expect("Cannot parse LOWERRIGHT");
     let mut pixels = vec![0; bounds.0 * bounds.1];
-    render(&mut pixels, bounds, upper_left, lower_right);
+
+    let threads = 2;
+    let rows_per_band = bounds.1 / threads + 1;
+
+    {
+        let bands = pixels
+            .chunks_mut(rows_per_band * bounds.0)
+            .collect::<Vec<_>>();
+
+        crossbeam::scope(|spawer| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len();
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right =
+                    pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+
+                spawer.spawn(move |_| {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        })
+        .unwrap();
+    }
+
     write_image(&args[1], &pixels, bounds).unwrap();
 }
